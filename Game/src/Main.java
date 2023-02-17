2b91c9e9ad2f50a51e01694944f18511c80d23fc
hkarilaid <h.karilaid@hotmail.com>
2023-02-17 15:36:14 +0200

    KLassid

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -6,7 +6,8 @@ public class Main {
     //fori teeb for tsükkli
     //sout system out
     public static void main(String[] args) {
-
+//        Random rand = new Random(); //uus instants
+//        Math.random(); // static
         System.out.println("Hello world!");
    //     String sonaline = "Sõnaline muutuja";
    //     char taheline = 's';
@@ -17,25 +18,20 @@ public class Main {
    //     short lyhikenumber = 123;
    //     long pikknumber = 12342342354L;
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input);
+        World world = new World(10,5);
+        Player player = new Player(world.width, world.height);
+        Dragon dragon = new Dragon(world.width, world.height);
+        Orc orc = new Orc(world.width, world.height);
 
-        int worldWidth = 10;
-        int worldHeight = 5;
-
-        Player player = new Player(worldWidth, worldHeight);
-        Dragon dragon = new Dragon(worldWidth, worldHeight);
-        Orc orc = new Orc(worldWidth, worldHeight);
-
-        printMap(worldWidth, worldHeight,
+        world.printMap(world.width, world.height,
                 player.xCoordinate, player.yCoordinate, player.symbol,
                 dragon.xCoordinate, dragon.yCoordinate, dragon.symbol,
                 orc.xCoordinate, orc.yCoordinate, orc.symbol);
-
+        String input = scanner.nextLine();
         while (!input.equals("end")) {
-            player.move(input);
+            player.move(input, world);
 
-            printMap(worldWidth, worldHeight,
+            world.printMap(world.width, world.height,
                     player.xCoordinate, player.yCoordinate, player.symbol,
                     dragon.xCoordinate, dragon.yCoordinate, dragon.symbol,
                     orc.xCoordinate, orc.yCoordinate, orc.symbol);
@@ -44,45 +40,5 @@ public class Main {
         }
     }
 
-    private static void printMap(int worldWidth, int worldHeight, int playerXcoordinate, int playerYcoordinate, char playerSymbol, int dragonXcoordinate, int dragonYcoordinate, char dragonSymbol, int orcXcoordinate, int orcYcoordinate, char orcSymbol) {
-        //kõrgus
-        // algväärtus    kuni    iga tsükkel
-        for (int y = 0; y < worldHeight; y++) {
-            System.out.println();
-            // laius
-            for (int x = 0; x < worldWidth; x++) {
-                if (y == 0 || y == worldHeight -1) {
-                    System.out.print("-");
-                } else if (x == 0 || x == worldWidth -1) {
-                    System.out.print("|");
-                } else {
 
-                    //parem klõps -> refactor -> extract method
-                    printCharacters(playerXcoordinate, playerYcoordinate, playerSymbol,
-                            dragonXcoordinate, dragonYcoordinate, dragonSymbol,
-                            orcXcoordinate, orcYcoordinate, orcSymbol, y, x);
-
-                }
-                //System.out.print(x);
-            }
-        }
-    }
-
-    private static int getRandomCoordinate(int worldDimension) {
-        return (int) (Math.random() * (worldDimension - 2) + 1);
-    }
-
-    private static void printCharacters(int playerXcoordinate, int playerYcoordinate, char playerSymbol,
-                                        int dragonXcoordinate, int dragonYcoordinate, char dragonSymbol,
-                                        int orcXcoordinate, int orcYcoordinate, char orcSymbol, int mapY, int mapX) {
-        if (playerXcoordinate == mapX && playerYcoordinate == mapY) {
-            System.out.print(playerSymbol);
-        } else if (dragonXcoordinate == mapX && dragonYcoordinate == mapY) {
-            System.out.print(dragonSymbol);
-        } else if (orcXcoordinate == mapX && orcYcoordinate == mapY) {
-            System.out.print(orcSymbol);
-        } else {
-            System.out.print(" ");
-        }
-    }
 }
--- Game/src/World.java
+++ Game/src/World.java
@@ -1,2 +1,55 @@
 public class World {
+
+    int width;
+    int height;
+
+    public World(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void printMap(int worldWidth, int worldHeight, int playerXcoordinate, int playerYcoordinate, char playerSymbol,
+                                 int dragonXcoordinate, int dragonYcoordinate, char dragonSymbol,
+                                 int orcXcoordinate, int orcYcoordinate, char orcSymbol) {
+        //kõrgus
+        // algväärtus    kuni    iga tsükkel
+        for (int y = 0; y < worldHeight; y++) {
+            System.out.println();
+            // laius
+            for (int x = 0; x < worldWidth; x++) {
+                if (y == 0 || y == worldHeight -1) {
+                    System.out.print("-");
+                } else if (x == 0 || x == worldWidth -1) {
+                    System.out.print("|");
+                } else {
+
+                    //parem klõps -> refactor -> extract method
+                    printCharacters(playerXcoordinate, playerYcoordinate, playerSymbol,
+                            dragonXcoordinate, dragonYcoordinate, dragonSymbol,
+                            orcXcoordinate, orcYcoordinate, orcSymbol, y, x);
+
+                }
+                //System.out.print(x);
+            }
+        }
+    }
+
+//    private static int getRandomCoordinate(int worldDimension) {
+//        return (int) (Math.random() * (worldDimension - 2) + 1);
+//    }
+
+    private void printCharacters(int playerXcoordinate, int playerYcoordinate, char playerSymbol,
+                                        int dragonXcoordinate, int dragonYcoordinate, char dragonSymbol,
+                                        int orcXcoordinate, int orcYcoordinate, char orcSymbol, int mapY, int mapX) {
+        if (playerXcoordinate == mapX && playerYcoordinate == mapY) {
+            System.out.print(playerSymbol);
+        } else if (dragonXcoordinate == mapX && dragonYcoordinate == mapY) {
+            System.out.print(dragonSymbol);
+        } else if (orcXcoordinate == mapX && orcYcoordinate == mapY) {
+            System.out.print(orcSymbol);
+        } else {
+            System.out.print(" ");
+        }
+    }
+
 }
