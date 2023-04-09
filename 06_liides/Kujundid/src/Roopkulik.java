1db923d9c1c595475cecf53e74276d83ff39e85b
hkarilaid <h.karilaid@hotmail.com>
2023-04-09 15:54:47 +0300

    Kodusen2ide

---
+++ 06_liides/Kujundid/src/Roopkulik.java
@@ -0,0 +1,17 @@
+public class Roopkulik extends Tasapinnaline implements Arvutused{
+
+
+    public Roopkulik(int tippudeArv) {
+        super(tippudeArv);
+    }
+
+    @Override
+    public double arvutaYmberMoot() {
+        return 0;
+    }
+
+    @Override
+    public double arvutaPindala() {
+        return 0;
+    }
+}
