97de205808446d978934dae11ce3de0fa7d03fc6
hkarilaid <h.karilaid@hotmail.com>
2023-04-27 14:54:12 +0300

    Controllerid

---
+++ 09_tunnirakenduse_lahendus/Primitiivid/src/main/java/ee/hendrik/primitiivid/Toode.java
@@ -0,0 +1,17 @@
+package ee.hendrik.primitiivid;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Toode {
+    private int id;
+    private String nimi;
+    private double hind;
+    private boolean aktiivne;
+}
